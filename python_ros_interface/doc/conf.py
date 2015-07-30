274fc7a55a466b05cdd420d9e0e5862871a43748
Tamaki Nishino <otamachan@gmail.com>
2015-07-30 23:26:44 +0900

    update

--- python_ros_interface/doc/conf.py
+++ python_ros_interface/doc/conf.py
@@ -3,19 +3,22 @@
 import os
 import sys
 import sphinx_rtd_theme
-import mock
 
-MOCK_MODULES = ['numpy', 'rospy', 'genpy', 'rosservice', 'rostopic', 'actionlib', 'tf2_ros', 'tf', 'tf.transformations']
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = mock.Mock()
-MESSAGE_MODULES = ['tf2', 'geometry', 'trajectory', 'std', 'sensor']
-for msg_name in MESSAGE_MODULES:
-    sys.modules[msg_name+'_msgs'] = mock.Mock()
-    sys.modules[msg_name+'_msgs.msg'] = mock.Mock()
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    import mock
+    mock_modules = ['numpy', 'rospy', 'genpy', 'rosservice', 'rostopic', 'actionlib', 'tf2_ros', 'tf', 'tf.transformations']
+    for mod_name in mock_modules:
+        sys.modules[mod_name] = mock.Mock()
+    mock_messages = ['tf2', 'geometry', 'trajectory', 'std', 'sensor']
+    for msg_name in mock_messages:
+        sys.modules[msg_name+'_msgs'] = mock.Mock()
+        sys.modules[msg_name+'_msgs.msg'] = mock.Mock()
 
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
 extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.napoleon']
 master_doc = 'index'
 project = u'python_ros_interface'
