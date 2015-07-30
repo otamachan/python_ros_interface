e96ed49c3ed9f807fea7ea88137967844a0649d7
Tamaki Nishino <otamachan@gmail.com>
2015-07-30 23:10:35 +0900

    update

--- python_ros_interface/doc/conf.py
+++ python_ros_interface/doc/conf.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
+
 import os
 import sys
 import sphinx_rtd_theme
+import mock
+
+MOCK_MODULES = ['numpy', 'rospy', 'genpy', 'rosservice', 'rostopic', 'actionlib', 'tf2_ros', 'tf', 'tf.transformations']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+MESSAGE_MODULES = ['tf2', 'geometry', 'trajectory', 'std', 'sensor']
+for msg_name in MESSAGE_MODULES:
+    sys.modules[msg_name+'_msgs'] = mock.Mock()
+    sys.modules[msg_name+'_msgs.msg'] = mock.Mock()
+
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
